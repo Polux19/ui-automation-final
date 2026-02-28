9aa723d3416ae5aab140cfd5d57c4a7c3b11d4cf
Belen <giangrassobelen@gmail.com>
2026-02-28 19:05:57 -0300

    Entrega Challenge Automation
    Puntos solicitados:
    
    Punto 1: Casos de Prueba en CasosDePrueba.md.
    
    Punto 2: Framework Java/Selenium/TestNG con capturas automáticas.
    
    Integración API: Se añade RestAssured para interactuar con servicios web.
    
    Punto 3: Tests de Login y Carrito en SauceDemoTest.java.
    
    Punto 4: Falla intencional en CP05 con evidencia en screenshots.
    
    Punto 5: Test de API en MercadoLibreApiTest.java validando departamentos de ML.

--- src/test/java/com/bel/automation/tests/MercadoLibreApiTest.java
+++ src/test/java/com/bel/automation/tests/MercadoLibreApiTest.java
@@ -11,9 +11,11 @@ public class MercadoLibreApiTest {
         String url = "https://www.mercadolibre.com.ar/menu/departments";
 
         RestAssured.given()
+                // Agregamos este "disfraz" para que ML no nos bloquee
+                .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/120.0.0.0")
                 .get(url)
                 .then()
-                .statusCode(200)
+                .statusCode(200) // Ahora sí debería devolver 200
                 .body("departments", notNullValue());
 
         System.out.println("API de Mercado Libre validada con éxito.");
